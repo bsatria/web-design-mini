131c75630634c05d9d6283949ed6592f2c327001
Bagas Satria <bagassatria040@gmail.com>
2020-02-28 01:28:35 +0700

    add redux thunk

---
+++ src/store/reducers/index.js
@@ -0,0 +1,4 @@
+import { name } from "./name";
+import { posts } from "./posts";
+
+export { name, posts };
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,18 @@
-import { createStore, combineReducers } from "redux";
-import { name } from "./reducers/name";
-import { posts } from "./reducers/posts";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import { name, posts } from "./reducers/";
 
 const reducers = combineReducers({
   name,
   posts
 });
 
-export const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+export const store = createStore(reducers, enhancer);
